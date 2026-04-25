ca69d5e423344f0e3f73d7fcb1d8698322cc665e
Pedro <pedro.sousa.pocas@gmail.com>
2026-04-25 17:36:53 +0100

    Em vez de encontrar o zero da função, vamos permitir que o utilizador insira um certo valor d

--- bolzanocauchy.py
+++ bolzanocauchy.py
@@ -1,7 +1,9 @@
 #import mathlib
-print("Há uma função f com um zero.\nDetermina um intervalo que tenha apenas um zero e seleciona o erro que desejas que apresente esse mesmo.")
+print("Há uma função f que interseta um certo valor d.\nDefina esse valor e o programa irá encontrar o intervalo mais próximo desse valor definido com a função:")
 def f(x):
     return 2**x -x**2
+print("f(x) = 2**x - x**2")
+d = int(input("d: "))
 erro = float(input("\nInsira o valor do erro: "))
 xmin = float(input("a: "))
 xmax = float(input("b: "))
@@ -23,14 +25,14 @@ while xmax - xmin > erro:
             xmax = pontomedio
             fmax = f(pontomedio)
             numiteracoes += 1
-            print("a")
+            #print("a")
             print("[", xmin, ",", xmax, "]")
 
         else: #b
             xmin = pontomedio
             fmin = f(pontomedio)
             numiteracoes += 1
-            print("b")
+            #print("b")
             print("[", xmin, ",", xmax, "]")
     elif fmax <0 and fmin > 0:
         amplitude = xmax - xmin
@@ -47,7 +49,7 @@ while xmax - xmin > erro:
             xmin = pontomedio
             fmin = f(pontomedio)
             numiteracoes += 1
-            print("d")
+            #print("d")
             print("[", xmin, ",", xmax, "]")
     else:
         print("O número mais próximo do zero da função é ", pontomedio) #DA ERRADO SE NAO TIVER ZEROS
@@ -55,4 +57,4 @@ while xmax - xmin > erro:
         break
 
 print("Número de iterações: ", numiteracoes)
-print("Está no intervalo: ", xmin, ";", xmax)
+print("Está no intervalo: ", "[", xmin, ";", xmax, "]")
