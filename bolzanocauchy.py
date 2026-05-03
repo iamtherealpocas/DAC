62d19cd37d8c91ade4d8fac6f8e54af89a236b5a
Poças <pedro.sousa.pocas@gmail.com>
2026-05-03 11:37:41 +0100

    O código já está pronto vou juntar para a main branch

--- bolzanocauchy.py
+++ bolzanocauchy.py
@@ -8,13 +8,20 @@ yaxis=0
 plt.rcParams["figure.figsize"] = [7.50, 3.50] #???
 plt.rcParams["figure.autolayout"] = True #FORMATAÇÃO DA JANELA
 x= np.linspace(-5, 5.5, 100) #NÃO TENHO A CERTEZA DO QUE ISTO FAZ? NÃO MEXER NO NÚMERO 100!!!
+xminfun=-2
+xmaxfun=5
+yminfun=-5
+ymaxfun=5
 
 plt.plot(x, f(x))
 plt.xlabel("x")
 plt.ylabel("y")
 plt.grid(True, alpha=0.3)
-plt.grid(axis="x", alpha=0.3) #NÃO ESTOU A CONSEGUIR METER OS EIXOS MESMO ESCREVENDO ISTO
-plt.grid(axis="y", alpha=0.3) #ISTO DEVE SER A SOLUÇÃO ----> https://pythonexamples.org/matplotlib-grid-axis-x/
+ax = plt.gca()
+ax.set_xlim([xminfun, xmaxfun])
+ax.set_ylim([yminfun, ymaxfun])
+plt.axhline(0, color='black', linewidth=.5)
+plt.axvline(0, color='black', linewidth=.5)
 
 plt.show() #SE DER ERRO TENS DE INSTALAR A PACKAGE "PyQT6" PARA O PROGRAMA CORRER O GRÁFICO!
 
